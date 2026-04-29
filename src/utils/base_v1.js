5f3c36b54ffd939a44c2ed7f72abf7485a696405
IEatPlates <kaizokuoninarulol@gmail.com>
2026-04-29 14:47:35 +0000

    erb

--- src/utils/base_v1.js
+++ src/utils/base_v1.js
@@ -1,2 +1,3 @@
-export const v1_base_url = "aniwatchtv.to";
+//export const v1_base_url = "aniwatchtv.to";
 //export const v1_base_url = "aniwatchtv.com.ro";
+export const v1_base_url = "anikai.to";
